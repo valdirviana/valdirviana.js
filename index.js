3c79fcee99dc563a6073b84d60b227ec3f9eaec7
Fernando Migliorini Tenorio <ftenorio@uolinc.com>
2019-05-06 10:40:39 -0300

    Fix bio and add Steam link

--- index.js
+++ index.js
@@ -3,12 +3,13 @@
 module.exports = () => ({
   name: 'Fernando M Tenório',
   picture: 'https://www.gravatar.com/avatar/9c62a4730ba4ebf764862b7866038dcb?s=300',
-  bio: 'Estou na área de TI há mais de 10 anos, sempre trabalhei com tecnologias web. No momento, atuo como engenheiro, procurando formas de tornar a empresa melhor.',
+  bio: 'Estou na área de TI a mais de 10 anos, sempre trabalhei com tecnologias web. Atualmente sou Engenheiro de Software Sênior na UOL/BoaCompra.',
   github: 'https://github.com/dotenorio',
   twitter: 'https://twitter.com/dotenorio',
-  linkedin: false,
+  linkedin: 'https://www.linkedin.com/in/dotenorio',
   facebook: 'https://www.facebook.com/dotenorio',
   telegram: 'https://telegram.me/dotenorio',
   email: 'dotenorio@gmail.com',
-  blog: 'https://blog.dotenorio.com'
+  blog: 'https://blog.dotenorio.com',
+  steam: 'https://steamcommunity.com/id/dotenorio'
 })
