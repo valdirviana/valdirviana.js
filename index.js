8215a7a0c3aa51df24ae9ca2a63684ddba58a1b4
Fernando Migliorini Tenório <dotenorio@gmail.com>
2019-06-17 09:07:31 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 module.exports = () => ({
   name: 'Fernando M Tenório',
   picture: 'https://www.gravatar.com/avatar/9c62a4730ba4ebf764862b7866038dcb?s=300',
-  bio: 'Estou na área de TI a mais de 10 anos, sempre trabalhei com tecnologias web. Atualmente sou Engenheiro de Software Sênior na UOL/BoaCompra.',
+  bio: 'Estou na área de TI já a mais de 10 anos, sempre trabalhei com tecnologias web e atualmente volto meus esforços para o Javascript, tanto front como backend. Atualmente trabalho como Desenvolvedor de Infraestrutura na Mandic (Rivendel) atuando basicamente no desenvolvimento javascript.',
   github: 'https://github.com/dotenorio',
   twitter: 'https://twitter.com/dotenorio',
   linkedin: 'https://www.linkedin.com/in/fernando-migliorini-877a29175',
