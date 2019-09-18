e6caf3ead638949546c98ebfef892b5df8acfb09
Fernando M Tenório <dotenorio@gmail.com>
2019-09-18 16:16:09 -0300

    fix: bio

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 
 module.exports = () => ({
-  name: 'Fernando M Tenório',
+  name: 'Fernando Migliorini Tenório',
   picture: 'https://www.gravatar.com/avatar/9c62a4730ba4ebf764862b7866038dcb?s=300',
-  bio: 'Estou na área de TI já a mais de 10 anos, sempre trabalhei com tecnologias web e atualmente volto meus esforços para o Javascript, tanto front como backend. Atualmente trabalho como Desenvolvedor de Infraestrutura na Mandic (Rivendel) atuando basicamente no desenvolvimento javascript.',
+  bio: 'Estou na área de TI já a mais de 10 anos, sempre trabalhei com tecnologias web e geralmente com Javascript, tanto para o front como para o backend. Atualmente trabalho como Desenvolvedor de Infraestrutura na Mandic (Rivendel).',
   github: 'https://github.com/dotenorio',
   twitter: 'https://twitter.com/dotenorio',
   linkedin: 'https://www.linkedin.com/in/fernando-migliorini-877a29175',
