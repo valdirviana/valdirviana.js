db5e7818859092d38826c773d0e6cb1ec0dcb800
Fernando Migliorini Tenório <dotenorio@gmail.com>
2020-01-06 14:26:55 -0300

    fix: bio

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 #!/usr/bin/env node
 
 module.exports = () => ({
-  name: 'Fernando Migliorini Tenório',
-  picture: 'https://www.gravatar.com/avatar/9c62a4730ba4ebf764862b7866038dcb?s=300',
-  bio: 'Estou na área de TI já a mais de 10 anos, sempre trabalhei com tecnologias web e geralmente com Javascript, tanto para o front como para o backend. Atualmente trabalho como Desenvolvedor de Infraestrutura na Mandic (Rivendel).',
-  github: 'https://github.com/dotenorio',
-  twitter: 'https://twitter.com/dotenorio',
-  linkedin: 'https://www.linkedin.com/in/dotenorio',
-  facebook: 'https://www.facebook.com/dotenorio',
-  telegram: 'https://telegram.me/dotenorio',
-  email: 'dotenorio@gmail.com',
-  blog: 'https://blog.dotenorio.com',
-  steam: 'https://steamcommunity.com/id/dotenorio'
-})
+  name: "Fernando Migliorini Tenório",
+  picture:
+    "https://www.gravatar.com/avatar/9c62a4730ba4ebf764862b7866038dcb?s=800",
+  bio:
+    "Estou na área de TI já a mais de 11 anos, sempre trabalhei com tecnologias web e geralmente com Javascript, tanto para o front como para o backend. Atualmente trabalho como Desenvolvedor de Software Sênior na Mandic.",
+  github: "https://github.com/dotenorio",
+  twitter: "https://twitter.com/dotenorio",
+  linkedin: "https://www.linkedin.com/in/dotenorio",
+  facebook: "https://www.facebook.com/dotenorio",
+  telegram: "https://telegram.me/dotenorio",
+  email: "dotenorio@gmail.com",
+  blog: "https://blog.dotenorio.com",
+  steam: "https://steamcommunity.com/id/dotenorio"
+});
