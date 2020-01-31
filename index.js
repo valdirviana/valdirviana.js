1d9b562853f11b2ee9be0c52478d7c886f90f061
Valdir Viana <viana.valdir@gmail.com>
2020-01-31 08:20:40 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
 #!/usr/bin/env node
 
 module.exports = () => ({
-  name: "Fernando Migliorini Tenório",
-  picture:
-    "https://www.gravatar.com/avatar/9c62a4730ba4ebf764862b7866038dcb?s=800",
-  bio:
-    "Estou na área de TI já a mais de 11 anos, sempre trabalhei com tecnologias web e geralmente com Javascript, tanto para o front como para o backend. Atualmente trabalho como Desenvolvedor de Software Sênior na Mandic.",
-  github: "https://github.com/dotenorio",
-  twitter: "https://twitter.com/dotenorio",
-  linkedin: "https://www.linkedin.com/in/dotenorio",
-  facebook: "https://www.facebook.com/dotenorio",
-  telegram: "https://telegram.me/dotenorio",
-  email: "dotenorio@gmail.com",
-  blog: "https://blog.dotenorio.com",
-  steam: "https://steamcommunity.com/id/dotenorio"
+  name: "Valdir Viana",
+  github: "https://github.com/valdirviana",
+  twitter: "https://twitter.com/valdir_viana",
+  linkedin: "https://www.linkedin.com/in/valdirviana",
+  telegram: "https://telegram.me/valdirviana",
+  email: "viana.valdir@gmail.com",
+  blog: "https://blog.valdirviana.now.sh/"
 });
